47542a3f0539bb4fda4e546e9b59cf1a8d8f2029
Tornai Heni <the.nr94@gmail.com>
2016-03-29 17:51:06 +0200

    Revert "Revert "2016.03.29. 16:08""
    This reverts commit fba293b3d1cb15d40fc83bfaf50e6172f78adeeb.

---
+++ urlap.js
@@ -0,0 +1,31 @@
+window.onload = indit;
+function indit(){
+
+   lista();
+
+}
+function lista(){
+  var i, selectv, szoveg, optionv, ldiv;
+  var selectv = document.createElement("select");
+  selectv.setAttribute("name","lista");
+  for(i=1960; i<=2005; i++){
+    var szoveg = document.createTextNode(i);
+    var optionv = document.createElement("option");
+    optionv.setAttribute("value",i);
+    optionv.appendChild(szoveg);
+    selectv.appendChild(optionv);
+  }
+  var ldiv = document.getElementById("listadiv");
+  ldiv.appendChild(selectv);
+}
+function kuldes(esemeny){
+  var elkuld = true;
+  elkuld = elkuld && confirm("Biztosan elküldöd?");
+  if(!elkuld){
+    if (typeof esemeny.preventDefault != "undefined"){
+      esemeny.preventDefault();
+    } else if(typeof window.event.returnValue != "undefined"){
+      window.event.returnValue = false;
+    } else { return false; }
+  }
+}
