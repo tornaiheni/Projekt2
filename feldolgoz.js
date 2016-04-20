73284cfa7ec0a3b9b578f724913282019ac0abca
Tornai Heni <the.nr94@gmail.com>
2016-04-20 17:45:13 +0200

    Kiiratás, kettőspont

--- feldolgoz.js
+++ feldolgoz.js
@@ -9,8 +9,11 @@ function feldolgoz(){
   for(var i=0; i<t.length;i++){
       t[i] = t[i].split("=");
       for(var j=0; j<t[i].length;j+=2){
-         t[i][j] = unescape(t[i][j]);
-		 t[i][j+1] = unescape(t[i][j+1]);
+         t[i][j]=t[i][j].split("_");
+               for(var k=0; k<t[i][j].length;k++){
+				t[i][j] = unescape(t[i][j]);
+				t[i][j+1] = unescape(t[i][j+1]);
+	 }
 
          adatsor +=t[i][j] +":"+ "&nbsp; "+t[i][j+1] + "&nbsp; ";
       }
